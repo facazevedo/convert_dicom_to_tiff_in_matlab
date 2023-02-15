c6bcfd40c2bc46bfbc217cc1372a6ccc9dde2bf1
Frederico A. C. Azevedo <fazevedo.lab@gmail.com>
2023-02-15 16:28:05 -0500

    Add files via upload

---
+++ convert_dicom_tiff.m
@@ -0,0 +1,26 @@
+% Instructions:
+% 1. Write the path of the folder where the dicom images are in "path_dcms"
+% 2. Write the desired path for the output tiff image in "path_tiff"
+% 3. Run the script
+path_dcms = 'C:\Users\fazevedo\Desktop\dicom';
+path_tiff = 'C:\Users\fazevedo\Desktop\dicom_tiff';
+
+% --- MAIN ---
+
+% If necessary, create "path_diff" or delete the previous image
+if ~exist(path_tiff, "dir")
+    mkdir(path_tiff);
+end
+
+% List all the dicom files inside the directory "path_dcms"
+list_dcms = dir(fullfile(path_dcms, "*.dcm"));
+
+for i = 1 : size(list_dcms,1)
+    tiff_img = dicomread(fullfile(path_dcms, list_dcms(i).name));
+    tiff_img = uint8(255 * mat2gray(tiff_img));
+    tiff_name = strrep(list_dcms(i).name, 'dcm', 'tiff');
+    if exist(fullfile(path_tiff, tiff_name), 'file')
+        delete(fullfile(path_tiff, tif_name));
+    end
+    imwrite(tiff_img, fullfile(path_tiff, tiff_name));
+end
